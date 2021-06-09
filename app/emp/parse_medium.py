030356f7329e2c9e3a7fb4ebb869d0c50f3867d0
FerrySchuller <ferry@f-inter.net>
2021-06-09 12:00:23 +0000

    commit by make git

--- app/emp/parse_medium.py
+++ app/emp/parse_medium.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import requests
 from pprint import pprint
 
 
@@ -6,9 +7,11 @@ soup = False
 with open("html") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
-print(soup)
-pprint(soup.title.name)
-pprint(soup.p)
+#url = 'https://medium.com/veve-collectibles/dc-cover-girls-series-1-c37f14a378ad'
+#r = requests.get(url)
+#soup = BeautifulSoup(r.content, 'html.parser')
 
-
-
+print(type(soup))
+#print(vars(soup))
+xo = soup.find_all("strong", class_="iz jt")
+pprint(xo)
