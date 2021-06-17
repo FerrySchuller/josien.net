a876f2077a7ba183f7eae28c553b7b2e1bd4a018
FerrySchuller <ferry@f-inter.net>
2021-06-17 11:32:48 +0000

    commit by make git

--- app/gen_epoch.py
+++ app/gen_epoch.py
@@ -5,3 +5,7 @@ print(int(datetime(2021, 5, 27, 17, 0).timestamp() * 1000))
 
 # Ron English - Cereal Killers - Series 1
 print(int(datetime(2021, 6, 10, 17, 0).timestamp() * 1000))
+
+# Superman — Series 1
+print(int(datetime(2021, 6, 17, 17, 0).timestamp() * 1000))
+
