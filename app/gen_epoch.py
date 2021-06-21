b1c81dc68e4cbac543b42d6d28477f772a395ec5
FerrySchuller <ferry@f-inter.net>
2021-06-21 09:31:46 +0000

    commit by make git

--- app/gen_epoch.py
+++ app/gen_epoch.py
@@ -1,13 +1,7 @@
 from datetime import datetime
 
-# DC Cover Girls — Series 1
-print(int(datetime(2021, 5, 27, 17, 0).timestamp() * 1000))
-
-# Ron English - Cereal Killers - Series 1
-print(int(datetime(2021, 6, 10, 17, 0).timestamp() * 1000))
-
-# Superman — Series 1
-print(int(datetime(2021, 6, 17, 17, 0).timestamp() * 1000))
-
-# Ghostbusters Slimer
-print(int(datetime(2021, 6, 18, 2, 0).timestamp() * 1000))
+print('{} {}'.format(int(datetime(2021, 5, 27, 17, 0).timestamp() * 1000), 'DC Cover Girls — Series 1'))
+print('{} {}'.format(int(datetime(2021, 6, 10, 17, 0).timestamp() * 1000), 'Ron English - Cereal Killers - Series 1'))
+print('{} {}'.format(int(datetime(2021, 6, 17, 17, 0).timestamp() * 1000), 'Superman — Series 1'))
+print('{} {}'.format(int(datetime(2021, 6, 18, 2, 0).timestamp() * 1000), 'Ghostbusters Slimer'))
+print('{} {}'.format(int(datetime(2021, 6, 21, 17, 0).timestamp() * 1000), 'Givenchy Beauty — PRIDE'))
