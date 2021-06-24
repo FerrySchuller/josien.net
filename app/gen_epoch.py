d2198ceeba668b71d92bd96d03a8b5e95e6a3dd4
FerrySchuller <ferry@f-inter.net>
2021-06-24 10:50:26 +0000

    commit by make git

--- app/gen_epoch.py
+++ app/gen_epoch.py
@@ -5,3 +5,4 @@ print('{} {}'.format(int(datetime(2021, 6, 10, 17, 0).timestamp() * 1000), 'Ron 
 print('{} {}'.format(int(datetime(2021, 6, 17, 17, 0).timestamp() * 1000), 'Superman — Series 1'))
 print('{} {}'.format(int(datetime(2021, 6, 18, 2, 0).timestamp() * 1000), 'Ghostbusters Slimer'))
 print('{} {}'.format(int(datetime(2021, 6, 21, 17, 0).timestamp() * 1000), 'Givenchy Beauty — PRIDE'))
+print('{} {}'.format(int(datetime(2021, 6, 24, 17, 0).timestamp() * 1000), 'DC Bombshells — Series 2'))
