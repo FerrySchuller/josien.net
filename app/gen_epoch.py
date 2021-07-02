eec4728b5706919f2c77327a57d23b3186ba8f2c
FerrySchuller <ferry@f-inter.net>
2021-07-02 07:44:09 +0000

    commit by make git

--- app/gen_epoch.py
+++ app/gen_epoch.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from dateparser import parse
 
-settings = {'TO_TIMEZONE': 'UTC'}
+settings = {'TO_TIMEZONE': 'GMT+1'}
 
 print('{} {}'.format(int(datetime(2021, 5, 27, 17, 0).timestamp() * 1000), 'DC Cover Girls — Series 1'))
 print('{} {}'.format(int(datetime(2021, 6, 10, 17, 0).timestamp() * 1000), 'Ron English - Cereal Killers - Series 1'))
@@ -12,8 +12,10 @@ print('{} {}'.format(int(datetime(2021, 6, 24, 17, 0).timestamp() * 1000), 'DC B
 
 
 
-l = [ {'title': 'Frank Kozik Labbit— Series 1', 'dt': 'Thursday, July 1st at 8 am PT'} ]
+l = [ { 'title': 'Frank Kozik Labbit— Series 1', 'dt': 'Thursday, July 1st at 8 am PT'},
+      { 'title': 'Back to the Future Part II Hoverboards — Series 1', 'dt': 'Thursday, July 1st at 6 pm PT'} ]
 
 for x in l:
     dt = parse(x['dt'], settings=settings)
+    #print(dt, x['title'])
     print("{{ x: {}, title: '{}' }}".format(int(dt.timestamp() * 1000), x['title']))
